e375d106132b74d01db082d0e7a4004c2cfafcba
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-28 09:04:09 -0800

    Add axiosWithAuth util and login component
    AxiosWithAuth was created to pass authorization on backend and login
    component for user with axiosWithAuth using post call

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
+  console.log(login);
+
+  const handleChanges = (event) => {
+    setLogin({ ...login, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //axios auth login post call to api
+    axiosWithAuth()
+      .post("/api/login", login)
+      .then((response) => {
+        //console.log(response);
+        window.localStorage.setItem("token", response.data.payload);
+      })
+      .catch((error) => {
+        //console.log(error);
+      });
+    //reset login form
+    setLogin({
+      username: "",
+      password: ""
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            name="username"
+            value={login.username}
+            onChange={handleChanges}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            name="password"
+            value={login.password}
+            onChange={handleChanges}
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
