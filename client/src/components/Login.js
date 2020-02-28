3e0362443792a2445f6fa427b99e667e9badcbac
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-28 09:22:03 -0800

    Made new util privateRoute
    This was to make a protected route with token that is returned from
    login post axiosWithAuth call

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
@@ -23,9 +23,10 @@ const Login = () => {
       .then((response) => {
         //console.log(response);
         window.localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubble-page");
       })
       .catch((error) => {
-        //console.log(error);
+        //console.log(error.response);
       });
     //reset login form
     setLogin({
